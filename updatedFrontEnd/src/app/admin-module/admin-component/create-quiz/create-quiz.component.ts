e64a110269afff20264945c486c5a4c2fd7440db
Arbaz <arbaz582021@gmail.com>
2023-03-30 11:14:25 +0500

    add

--- updatedFrontEnd/src/app/admin-module/admin-component/create-quiz/create-quiz.component.ts
+++ updatedFrontEnd/src/app/admin-module/admin-component/create-quiz/create-quiz.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Question } from 'src/app/shared/Interface/quizInterface';
-import { QuizService } from 'src/app/shared/services/quiz.service';
 
 @Component({
   selector: 'app-create-quiz',
@@ -9,44 +7,40 @@ import { QuizService } from 'src/app/shared/services/quiz.service';
   styleUrls: ['./create-quiz.component.css']
 })
 export class CreateQuizComponent implements OnInit {
-    questionForm: FormGroup|any;
-  
-    constructor(
-      private formBuilder: FormBuilder,
-      private _createQuizSerice:QuizService
-      ) {}
-  
-    ngOnInit(): void {
-      this.questionForm = this.formBuilder.group({
-        description: ['', Validators.required],
-        alternatives: this.formBuilder.array([
-          this.createAlternativeFormGroup(),
-          this.createAlternativeFormGroup(),
-          this.createAlternativeFormGroup(),
-          this.createAlternativeFormGroup(),
-        ]),
-      });
-    }
-  
-    onSubmit(): void {
-      const question: Question = {
-        description: this.questionForm.value.description,
-        alternatives: this.questionForm.value.alternatives,
-        softDelete: 0,
-      };
-  
-      console.log('New question:', question);
-      this._createQuizSerice.createQuiz(question).subscribe(((res:any)=>{
-        console.log( res.Body);
-      }))
-    }
-  
-    private createAlternativeFormGroup(): FormGroup {
-      return this.formBuilder.group({
-        text: ['', Validators.required],
-        isCorrect: [false],
-      });
-    }
-  
+  questionForm: FormGroup|any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.questionForm = this.fb.group({
+      quize: this.fb.array([
+        this.fb.group({
+          subject: ['', Validators.required],
+          organization: ['', Validators.required],
+          setName: ['', Validators.required],
+          description: ['', Validators.required],
+          alternatives: this.fb.array([
+            this.generateAlternative(),
+            this.generateAlternative(),
+            this.generateAlternative(),
+            this.generateAlternative()
+          ])
+        })
+      ])
+    });
+    console.log(this.questionForm.value)
+  }
+
+  generateAlternative() {
+    return this.fb.group({
+      text: ['', Validators.required],
+      isCorrect: [false]
+    });
+  }
+
   
 }
