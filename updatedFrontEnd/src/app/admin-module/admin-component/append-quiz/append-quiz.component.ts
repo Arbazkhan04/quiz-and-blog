69b867e842275e71fff1ac9fd1b380ab412024c5
Arbaz <arbaz582021@gmail.com>
2023-03-31 08:19:11 +0500

    add

--- updatedFrontEnd/src/app/admin-module/admin-component/append-quiz/append-quiz.component.ts
+++ updatedFrontEnd/src/app/admin-module/admin-component/append-quiz/append-quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { elementAt } from 'rxjs';
 import { QuizService } from 'src/app/shared/services/quiz.service';
 
@@ -8,22 +9,59 @@ import { QuizService } from 'src/app/shared/services/quiz.service';
   styleUrls: ['./append-quiz.component.css']
 })
 export class AppendQuizComponent {
+
   organizationName = ["ecat", "net"];
   subjectName = [];
-  setNumber=[]
+  setNumber = [];
+  questionObjectentity: FormGroup | any;
 
-  constructor(private _quizeServie: QuizService) { }
 
-  onChageFetchDataFromDropDown(event:any) {
-    let value=event.target.value;
-    this._quizeServie.appendquiz(value).subscribe((res:any)=>{
-      const mySubjectNameArr = res.Result[0].quize.map((element:any) => element.subject);
+  constructor(
+    private _quizeServie: QuizService,
+    private fb: FormBuilder
+  ) { this.buildForm() }
+
+
+  buildForm() {
+    this.questionObjectentity = this.fb.group({
+      subject: ['', Validators.required],
+      organization: ['', Validators.required],
+      setName: ['', Validators.required],
+      description: ['', Validators.required],
+      alternatives: this.fb.array([
+        this.generateAlternative(),
+        this.generateAlternative(),
+        this.generateAlternative(),
+        this.generateAlternative(),
+      ])
+    })
+  }
+
+
+  generateAlternative() {
+    return this.fb.group({
+      text: ['', Validators.required],
+      isCorrect: [false]
+    })
+  }
+
+
+  submitForm() {
+    console.log(this.questionObjectentity.value)
+  }
+
+
+  onChageFetchDataFromDropDown(event: any) {
+    let value = event.target.value;
+    this._quizeServie.appendquiz(value).subscribe((res: any) => {
+      const mySubjectNameArr = res.Result[0].quize.map((element: any) => element.subject);
       this.subjectName = mySubjectNameArr;
       console.log(mySubjectNameArr); // check the contents of myarr
-      const mySetNumber=res.Result[0].quize.map((item:any)=>item.setName);
-      this.setNumber=mySetNumber;
+      const mySetNumber = res.Result[0].quize.map((item: any) => item.setName);
+      this.setNumber = mySetNumber;
       console.log(mySetNumber)
     });
   }
   
+
 }
