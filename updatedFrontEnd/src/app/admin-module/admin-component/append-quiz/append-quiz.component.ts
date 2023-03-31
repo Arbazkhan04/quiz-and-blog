a741ad63caa1ae9241a6b59280315204885de720
Arbaz <arbaz582021@gmail.com>
2023-03-31 06:18:08 +0500

    add

--- updatedFrontEnd/src/app/admin-module/admin-component/append-quiz/append-quiz.component.ts
+++ updatedFrontEnd/src/app/admin-module/admin-component/append-quiz/append-quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 import { QuizService } from 'src/app/shared/services/quiz.service';
 
 @Component({
@@ -7,18 +8,22 @@ import { QuizService } from 'src/app/shared/services/quiz.service';
   styleUrls: ['./append-quiz.component.css']
 })
 export class AppendQuizComponent {
- organizationName=["ecat","net"]
+  organizationName = ["ecat", "net"];
+  subjectName = [];
+  setNumber=[]
+
+  constructor(private _quizeServie: QuizService) { }
+
+  onChageFetchDataFromDropDown(event:any) {
+    let value=event.target.value;
+    this._quizeServie.appendquiz(value).subscribe((res:any)=>{
+      const mySubjectNameArr = res.Result[0].quize.map((element:any) => element.subject);
+      this.subjectName = mySubjectNameArr;
+      console.log(mySubjectNameArr); // check the contents of myarr
+      const mySetNumber=res.Result[0].quize.map((item:any)=>item.setName);
+      this.setNumber=mySetNumber;
+      console.log(mySetNumber)
+    });
+  }
   
- constructor(private _quizeServie:QuizService)
- {}
-
- onChageFetchDataFromDropDown(event:any)
- {
-  let value=event.target.value;
-  this._quizeServie.appendquiz(value).subscribe((res)=>{
-    res
-  })
- }
-
-
 }
